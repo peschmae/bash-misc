2ef3993caf7dd5e4fb720445b5ce2855cd8aca21
mathias petermann <mathias.petermann@students.fhnw.ch>
2021-02-25 13:42:08 +0100

    Update doc-sync, add owncloud script

--- fhnw/doc-sync.sh
+++ fhnw/doc-sync.sh
@@ -1,68 +1,30 @@
 #! /bin/bash
 
-for i in "$@"
-do
-case $i in
-    -d=*|--directories-file=*)
-    DIRECTORIES="${i#*=}"
-    shift # past argument=value
-    ;;
-    -t=*|--target=*)
-    TARGET="${i#*=}"
-    shift # past argument=value
-    ;;
-    *)
-          # unknown option
-    ;;
-esac
-done
+SEMESTER=2021-fs
 
-if [ -z "$DIRECTORIES" ] || [ ! -f "$DIRECTORIES" ]
-then
-  echo "No directories provided"
-  exit 1
-else
-  directory_list=$(cat "$DIRECTORIES")
-  # echo "Synching folders: $directory_list"
-fi
+DESTINATION="/mnt/fhnw/$SEMESTER"
 
-if [ -z "$TARGET" ]
-then
-  echo "No target provided"
-  exit 1
-else
-  echo "Syncing to $TARGET"
-fi
-
-# get file with module directories, from a text file, -d/--directories parameter
-
-
-# first make sure the NFS share is mounted
-mount -a
-
-if [ ! -d "$TARGET" ]
-then
-  echo "Target doesn't exist"
-  exit 1
-fi
+MAX_FILE_SIZE=100M
+BASE_SOURCE=/mnt/fhnw-share
+PATH_KONTEXT=/mnt/fhnw-share/E1811_Unterrichte_Kontext
+PATH_I=/mnt/fhnw-share/E1811_Unterrichte_I
 
 # connect to the VPN, using the password file
 openconnect -b --user="dilli.gaf@students.fhnw.ch" vpn.fhnw.ch < /tmp/vpn-credentials
 
+
 # mount fhnw-share
-mount -t cifs //fs.edu.ds.fhnw.ch/data/HT /mnt/fhnw-share -o credentials=/tmp/mnt-credentials
+mount -t cifs //fs.edu.ds.fhnw.ch/data/HT/E1811_Unterrichte_Bachelor/ /mnt/fhnw-share -o credentials=/tmp/fhnw-credentials
 
 # rsync a list of folders defined above
-OLDIFS=$IFS
-IFS=';'
-while read -r dest source
-do
-  echo "synching $dest to $source"
-  echo rsync -rltD "/mnt/fhnw-share/$source/" "/mnt/fhnw/$dest"
-  rsync -rltD --exclude=".[!.]*" "/mnt/fhnw-share/$source/" "/mnt/fhnw/$dest"
-done <<< "$directory_list"
-IFS=$OLDIFS
+echo "synching $PATH_KONTEXT to $DESTINATION"
+echo rsync -rltD --exclude=".[!.]*" --max-size=50M "$BASE_SOURCE/$PATH_KONTEXT/*" "$DESTINATION/kontext"
+rsync -rltD --exclude=".[!.]*" --max-size="$MAX_FILE_SIZE" "$BASE_SOURCE/$PATH_KONTEXT/*" "$DESTINATION/kontext"
 
-# unmount share & kill vpn
+echo "synching $PATH_I to $DESTINATION"
+echo rsync -rltD --exclude=".[!.]*" --max-size=50M "$BASE_SOURCE/$PATH_I/" "$DESTINATION/informatik"
+rsync -rltD --exclude=".[!.]*" --max-size="$MAX_FILE_SIZE" "$BASE_SOURCE/$PATH_I/" "$DESTINATION/informatik"
+
+# kill vpn
 umount /mnt/fhnw-share
 pkill -SIGINT openconnect
---
+++ fhnw/owncloud-sync.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+## Change this!
+MODULE=bsys
+CLASS=2Ia
+# either informatik or kontext
+MODULE_TYPE=informatik
+SEMESTER=2021-fs
+
+## OwnCloud related
+DOWNLOAD_URL=https://drive.switch.ch/index.php/s/<DOWNLOAD>/download
+MAIN_FOLDER=Betriebssysteme
+
+## FIXED VARS
+MNT_FHNW="/mnt/fhnw"
+SYNC_DEST="$MNT_FHNW/$SEMESTER/$MODULE_TYPE/$CLASS/$MODULE"
+
+wget -O "$MODULE-sync.zip" "$DOWNLOAD_URL"
+
+unzip "$MODULE-sync.zip"
+
+cd $MAIN_FOLDER || exit
+
+rsync -rl . "$SYNC_DEST"
+
+# CLEANUP
+cd .. || exit
+rm -rf "$MAIN_FOLDER"
+rm "$MODULE-sync.zip"
